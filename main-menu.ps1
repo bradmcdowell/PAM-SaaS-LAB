d6d5983d63ea69469538ff11002520f30e363873
Brad McDowell <brad@mcdowell.com.au>
2025-12-10 13:45:02 +1100

    Add Get IP

--- main-menu.ps1
+++ main-menu.ps1
@@ -7,8 +7,9 @@ function Show-Menu {
     Write-Host "1. Set Mobile/Cell and Email for ACME LAB users"
     Write-Host "2. Set Region and Time-Zone for all Servers"
     Write-Host "3. Set Light or Dark mode"
-    Write-Host "4. Test CCP"
-    Write-Host "5. Exit"
+    Write-Host "4  Get Egress Public IP"
+    Write-Host "5. Test CCP"
+    Write-Host "6. Exit"
     Write-Host
 }
 
@@ -21,8 +22,9 @@ function Run-Menu {
             "1" { & ".\Update-SMS-EMAIL\Update-SMS-Email.ps1"; Read-Host "Press Enter to continue" }
             "2" { & ".\Theme\Update-TimeZone-Region.ps1"; Read-Host "Press Enter to continue" }
             "3" { & ".\Theme\Set-WindowsTheme.ps1"; Read-Host "Press Enter to continue" }
-            "4" { & ".\LABs\Migration\Test-CCP.ps1"; Read-Host "Press Enter to continue" }
-            "5" { Write-Host "Exiting..." -ForegroundColor Yellow; return }  # Exits the function and menu
+            "4" { & "iwr ipinfo.io"; Read-Host "Press Enter to continue" }
+            "5" { & ".\LABs\Migration\Test-CCP.ps1"; Read-Host "Press Enter to continue" }
+            "6" { Write-Host "Exiting..." -ForegroundColor Yellow; return }  # Exits the function and menu
             default { Write-Host "Invalid choice"; Start-Sleep -Seconds 1 }
         }
     }
